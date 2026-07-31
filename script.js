6cceb3bb90331689a3d25390467a595864ed3b6b
Meruya-dell <alclsimeruya@gmail.com>
2026-07-31 13:35:19 +0700

    done edit

--- script.js
+++ script.js
@@ -126,7 +126,6 @@ function scanBarcode() {
     
 
             function (decodedText) {
-                alert("TERBACA : " + decodedText);
                 txt.value = decodedText;
 
                 beep();
